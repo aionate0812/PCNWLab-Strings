afc4207e47b706e2d1ca165f12e0a47fc272f1ec
Alexander <aionate0812@gmail.com>
2018-10-16 05:37:46 -0400

    pset1 solutions

--- pset1.js
+++ pset1.js
@@ -3,8 +3,9 @@
  *  For the problems below, use only string methods / conditionals, etc
  *  it may be tempting to use concepts like looping but do not attempt! 
  */
- 
- 
+const isVowel = (str) => "aeiou".lastIndexOf(str.toLowerCase()) >= 0 ? true : false
+
+
 /* 1
     @func makeEven
     @param {string} str
@@ -15,8 +16,11 @@
     @example - makeEven('taq');  // 'taq#'
     @example - makeEven('john'); // 'john'
 */
+const makeEven = (string) => (string.length % 2) === 0 ? string : string + "#"
 
 
+console.log(makeEven("taq"))
+console.log(makeEven("john"))
 /* 2
     @func getLastChar
     @param {string} str
@@ -24,7 +28,10 @@
     @desc - return the last character in string
     @example - getLastChar('taq'); // 'q'
 */
+const getLastChar = (str) => str[str.length - 1]
 
+
+console.log(getLastChar("taq"))
 /* 3
     @func get3rdChar
     @param {string} str
@@ -33,7 +40,11 @@
     @example - get3rdChar('taq'); // 'q'
     @example - get3rdChar('mo'); // 'not enough characters!'
 */
+const get3rdChar = (str) => str.length > 2 ? str[2]: "not enough characters"
 
+
+console.log(get3rdChar('taq'))
+console.log(get3rdChar('mo'))
 /* 4
     @func get3rdCharFromEnd
     @param {string} str
@@ -43,7 +54,13 @@
     @example - get3rdCharFromEnd('mo'); // 'not enough characters!'
     @example - get3rdCharFromEnd('taq karim'); // 'r'
 */
+const get3rdCharFromEnd = (str) => str.length > 2 ? str[str.length - 3]: "not enough characters"
 
+
+
+console.log(get3rdCharFromEnd('taq'))
+console.log(get3rdCharFromEnd('mo'))
+console.log(get3rdCharFromEnd('taq karim'))
 /* 5
     @func startsWithVowel
     @param {string} str
@@ -54,6 +71,14 @@
     @example - startsWithVowel('andy'); // true
     @example - startsWithVowel('Andy'); // true
 */
+const startsWithVowel = (str) => isVowel(str[0]) ? true: false
+
+
+console.log("startsWithVowel")
+console.log(startsWithVowel('taq'))
+console.log(startsWithVowel('Dfghjkl'))
+console.log(startsWithVowel('andy'))
+console.log(startsWithVowel('Andy'))
 
 /* 6
     @func endsWithVowel
@@ -66,6 +91,14 @@
     @example - endsWithVowel('AndI'); // true
 */
 
+const endsWithVowel = (str) => isVowel(str[str.length - 1]) ? true : false
+
+
+console.log("startsWithVowel")
+console.log(startsWithVowel('taq'))
+console.log(startsWithVowel('Dfghjkl'))
+console.log(startsWithVowel('andi'))
+console.log(startsWithVowel('AndI'))
 
 /* 7
     @func hasVowels
@@ -77,7 +110,18 @@
     @example - hasVowels('dfghjkl'); // false
     @example - hasVowels('taq karim'); // true
 */
+const hasVowels = (str) => {
+    if (str.toLowerCase().includes('a') || str.toLowerCase().includes('e') || str.toLowerCase().includes('i') || str.toLowerCase().includes('o') || str.toLowerCase().includes('u')) {
+        return true
+    }
+    return false
+}
 
+console.log("hasVowels")
+console.log(hasVowels('taq'))
+console.log(hasVowels('TAQ'))
+console.log(hasVowels('dfghjkl'))
+console.log(hasVowels('taq karim'))
 
 /* 8
     @func hasUpperCase
@@ -89,7 +133,14 @@
     @example - hasUpperCase('tag'); // false
     @example - hasUpperCase('taq karim'); // false
 */
+const hasUpperCase = (str) => str === str.toLowerCase() ? false : true
 
+
+console.log("hasUpperCase")
+console.log(hasUpperCase('tAq'))
+console.log(hasUpperCase('TAQ'))
+console.log(hasUpperCase('tag'))
+console.log(hasUpperCase('taq karim'))
 /* 9
     @func isPalindromic
     @param {string} str
@@ -100,7 +151,20 @@
     @example - isPalindromic('bats see bees stab'); // false
     @example - hasUpperCase('taq karim'); // false
 */
+const isPalindromic = (str) => {
+    arr = str.split("")
+    arrReverse = arr.reverse()
+    if (str === arrReverse.join("")) {
+        return true
+    }
+    return false
+}
 
+console.log("isPalindromic")
+console.log(isPalindromic('racecar'))
+console.log(isPalindromic('racecars'))
+console.log(isPalindromic('bats see bees stab'))
+console.log(isPalindromic('taq karim'))
 /* 10
     @func getInitials
     @param {string} str
@@ -113,7 +177,11 @@
     @example - getInitials('John       Smith'); // "js"
     @example - getInitials('sue RonaN'); // "sr"
 */
+const getInitials = (str) => str[0] + str[(str.lastIndexOf(" ")) + 1]
+    // return str[0]+str[(str.search(/\s\w/))+1])
 
+console.log("getInitials")
+console.log(getInitials('taq     karim'))
 /* 11
     @func isPerfectStr
     @param {string} str
@@ -129,6 +197,35 @@
     @example - isPerfectStr('asdfaeKeccccc'); // true
 */
 
+
+const isPerfectStr = (str) => {
+    const length = str.length
+    const middle = Math.floor(length / 2)
+    if (length > 2) {
+        if (!isVowel(str[middle]) !== false) {
+            if (isVowel(str[middle - 1])) {
+                if (isVowel(str[middle + 1])) {
+                    if (isPalindromic(str[middle - 1] + str[middle] + str[middle + 1])) {
+                        return true
+                    }
+                    return false
+                }
+                return false
+            }
+            return false
+        }
+        return false
+    }
+    return false
+
+}
+
+console.log("isPerfectStr")
+console.log(isPerfectStr('ab')) // false
+console.log (isPerfectStr('aba')) // true
+console.log(isPerfectStr('asdfaeKeccc')) // false
+console.log(isPerfectStr('asdfaeKeccccc')) // true
+
 /* 12
     @func strMasher
     @param {string} str1
@@ -140,3 +237,9 @@
     @example - capitalLast('hello', 'wrold'); // "helwroldlo"
 */
 
+const strMasher = (str1,str2) => `${str1.substring(0,Math.ceil(str1.length / 2))}${str2}${str1.substring(Math.ceil(str1.length / 2))}`
+
+
+console.log("strMasher")
+console.log(strMasher('help', 'me')) // "hemelp"
+console.log(strMasher('hello', 'wrold')) // "helwroldlo"
